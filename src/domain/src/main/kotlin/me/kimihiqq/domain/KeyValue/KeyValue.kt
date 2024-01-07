6b7f53716b286c5334265627183f0d68634013d5
kimihiqq <kimihiqq@gmail.com>
2024-01-08 03:51:16 +0900

    feat: KeyValue 클래스 JPA 애너테이션 적용
    - 객체와 데이터베이스 테이블 간의 매핑을 구현하기위해 KeyValue 엔티티 클래스에 JPA 애너테이션을 추가했습니다.
    - JPA 적용을 위한 관련 의존성들을 추가했습니다.
    - 사용되지 않는 단위 테스트 및 Main 클래스를 제거했습니다.

--- src/domain/src/main/kotlin/me/kimihiqq/domain/KeyValue/KeyValue.kt
+++ src/domain/src/main/kotlin/me/kimihiqq/domain/KeyValue/KeyValue.kt
@@ -1,12 +1,17 @@
 package me.kimihiqq.domain.KeyValue
 
-data class KeyValue(
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "key_value")
+class KeyValue(
+    @Id @Column(name = "key_column")
     val key: String,
+    @Column(name = "value_column")
     var value: String?
 ) {
-    init {
-        require(key.isNotBlank()) { "키는 비어 있을 수 없습니다" }
-        require(key.length <= 50) { "키 길이는 50자를 넘을 수 없습니다" }
-        require(key.matches(Regex("[a-zA-Z0-9_]+"))) { "키는 알파벳, 숫자, 밑줄만 포함할 수 있습니다" }
-    }
+    constructor() : this("", null)
 }
