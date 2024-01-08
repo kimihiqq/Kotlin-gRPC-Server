4ca9635d32f7358a488c6a405f95270eca6b06df
kimihiqq <kimihiqq@gmail.com>
2024-01-08 09:40:09 +0900

    feat: 구동환경에 JPA 적용
    - JPA로 변경된 저장로직을 구동환경에 적용하였습니다.
    - 테이블 명 및 필드 명을 변경하였습니다.

--- src/infrastructure/boot/src/main/kotlin/me/kimihiqq/boot/Main.kt
+++ src/infrastructure/boot/src/main/kotlin/me/kimihiqq/boot/Main.kt
@@ -2,19 +2,24 @@ package me.kimihiqq.boot
 
 import io.grpc.ServerBuilder
 import me.kimihiqq.application.service.KeyValueService
-import me.kimihiqq.boot.config.DataSourceConfig
 import me.kimihiqq.presentation.serviceImpl.KeyValueServiceImpl
-import me.kimihiqq.provider.repository.impl.JdbcKeyValueRepository
+import me.kimihiqq.provider.config.EntityManagerConfig
+import me.kimihiqq.provider.repository.impl.JpaKeyValueRepository
+import java.util.concurrent.Executors
+import javax.persistence.Persistence
 
 fun main() {
-    val dataSourceConfig = DataSourceConfig()
-    val dataSource = dataSourceConfig.createDataSource()
-    val keyValueRepository = JdbcKeyValueRepository(dataSource)
+
+    EntityManagerConfig.initialize("KeyValuePersistenceUnit")
+
+    val keyValueRepository = JpaKeyValueRepository()
     val keyValueService = KeyValueService(keyValueRepository)
     val keyValueServiceImpl = KeyValueServiceImpl(keyValueService)
+    val executorService = Executors.newFixedThreadPool(10)
     val server = ServerBuilder
         .forPort(8080)
         .addService(keyValueServiceImpl)
+        .executor(executorService)
         .build()
 
     server.start()
